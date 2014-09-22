a88368ed592f776f1b3234e98060e64ba923232d
jbzjhan <jbzjhan@gmail.com>
2014-09-22 17:07:49 +0800

    September 22, 2014

---
+++ PlotCharts/PlotCharts/DataCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlotCharts
+{
+    class DataCollection
+    {
+        public int Step { get; set; }
+
+        public double Temperature { get; set; }
+
+        public double Time { get; set; }
+
+        public string Unit { get; set; }
+
+        public string Pause { get; set; }
+    }
+}
