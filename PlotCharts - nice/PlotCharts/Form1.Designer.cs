4642926cc665add342fdb9f7a1fcf3b03fd60e76
jbzjhan <jbzjhan@gmail.com>
2014-09-24 13:17:06 +0800

    September 24, 2014

---
+++ PlotCharts - nice/PlotCharts/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace PlotCharts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea2.AxisX.Interval = 0.5D;
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisX.ScrollBar.BackColor = System.Drawing.Color.White;
+            chartArea2.AxisX.ScrollBar.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            chartArea2.AxisX.ScrollBar.ButtonStyle = System.Windows.Forms.DataVisualization.Charting.ScrollBarButtonStyles.ResetZoom;
+            chartArea2.AxisX.ScrollBar.IsPositionedInside = false;
+            chartArea2.AxisX.ScrollBar.LineColor = System.Drawing.Color.Gray;
+            chartArea2.AxisX.ScrollBar.Size = 12D;
+            chartArea2.AxisX.Title = "Time";
+            chartArea2.AxisY.ScrollBar.BackColor = System.Drawing.Color.White;
+            chartArea2.AxisY.ScrollBar.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            chartArea2.AxisY.ScrollBar.ButtonStyle = System.Windows.Forms.DataVisualization.Charting.ScrollBarButtonStyles.ResetZoom;
+            chartArea2.AxisY.ScrollBar.IsPositionedInside = false;
+            chartArea2.AxisY.ScrollBar.LineColor = System.Drawing.Color.Silver;
+            chartArea2.AxisY.ScrollBar.Size = 12D;
+            chartArea2.AxisY.Title = "Temp";
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            legend2.Title = "Legend";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 11);
+            this.chart1.Name = "chart1";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Red;
+            series2.Legend = "Legend1";
+            series2.Name = "Temperature";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(679, 364);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            title2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title2.Name = "Title1";
+            title2.Text = "Graph for Temperature";
+            this.chart1.Titles.Add(title2);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 381);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(679, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReset.Location = new System.Drawing.Point(553, 255);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(116, 21);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPrint.Location = new System.Drawing.Point(553, 282);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(116, 21);
+            this.btnPrint.TabIndex = 5;
+            this.btnPrint.Text = "Print Graph";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(553, 309);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(116, 21);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save Graph";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 538);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Graph Program";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ PlotCharts - nice/PlotCharts/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using OpcRcw.Da;
+
+namespace PlotCharts
+{
+    public partial class Form1 : Form
+    {
+
+        DxpSimpleAPI.DxpSimpleClass opc = new DxpSimpleAPI.DxpSimpleClass();
+        public int count=0, compTemp, totalCount=20;
+        public double currentTemp, nextTemp, currentTime = 0, decimalValue, wholeValue;
+        public bool firstStep = true;
+        const int TEM_POS = 1500;
+        const int MST_POS = 1550;
+        const int LOWER_TIME_POS = 1600;
+        const int UNIT_BIT_POS = 1500;
+        const int STEP_OFFSET = 50;
+        const double minValue = -100.0;
+        const double maxValue = 100.0;
+        const string DEV_NAME = "DEV1";
+        const string WORD_REG_PREFIX = "D";
+        const string BIT_REG_PREFIX = "M";
+        const int MAX_DIGIT = 10000;
+        List<DataCollection> data = new List<DataCollection> { };
+        public Form1()
+        {
+            InitializeComponent(); 
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            if (opc.Connect("localhost", "Takebishi.dxp"))
+            {
+                chart1.ChartAreas[0].AxisX.ScaleView.Zoom(0, 10);
+                chart1.ChartAreas[0].AxisY.ScaleView.Zoom(0, 15);
+                dataGridView1.DefaultCellStyle.SelectionBackColor = dataGridView1.DefaultCellStyle.BackColor;
+                dataGridView1.DefaultCellStyle.SelectionForeColor = dataGridView1.DefaultCellStyle.ForeColor;
+                ReadValues();
+                totalCount = data.Count;
+                dataGridView1.DataSource = data;
+                for (; count < totalCount; count++)
+                {
+                    if (Convert.ToDouble(dataGridView1.Rows[count].Cells[2].Value) != 0)
+                    {
+                        //first value
+                        currentTemp = Convert.ToDouble(dataGridView1.Rows[count].Cells[1].Value);
+                        chart1.Series["Temperature"].Points.AddXY(currentTime, currentTemp);
+
+                        //plot whole the next step
+                        string unit = dataGridView1.Rows[count].Cells[3].Value.ToString();
+                        if (("second").Equals(unit))
+                        {
+                            currentTime = currentTime + secToHour(Convert.ToInt32(dataGridView1.Rows[count].Cells[2].Value));
+                        }
+                        else if (("minute").Equals(unit))
+                        {
+                            currentTime = currentTime + minToHour(Convert.ToInt32(dataGridView1.Rows[count].Cells[2].Value));
+                        }
+                        else
+                        {
+                            currentTime = currentTime + Convert.ToInt32(dataGridView1.Rows[count].Cells[2].Value);
+                        }
+                        chart1.Series["Temperature"].Points.AddXY(currentTime, currentTemp);
+
+                        //search for next value
+                        count++;
+                        for (; count < totalCount; count++)
+                        {
+                            if (Convert.ToInt32(dataGridView1.Rows[count].Cells[2].Value) != 0)
+                            {
+                                nextTemp = Convert.ToDouble(dataGridView1.Rows[count].Cells[1].Value);
+                                compTemp = (currentTemp > nextTemp) ? -10 : 5;
+                                count--;
+                                break;
+                            }
+                        }
+
+                        //perform calculation                    
+                        for (; currentTemp != nextTemp; )
+                        {
+                            if (currentTemp >= nextTemp && compTemp == 5)
+                            {
+                                if (currentTemp != nextTemp)
+                                {
+                                    not_Enough();
+                                    currentTemp += 1;
+                                    for (; currentTemp <= nextTemp; currentTemp = currentTemp + 1)
+                                    {
+                                        currentTime += 0.2;
+                                        chart1.Series["Temperature"].Points.AddXY(currentTime, currentTemp);
+                                    }
+                                    currentTemp -= 1;
+                                    nextTemp = nextTemp + decimalValue;
+                                    currentTemp = (decimalValue != 0) ? currentTemp + 0.1 : currentTemp;
+                                    for (; currentTemp < nextTemp; currentTemp = currentTemp + 0.1)
+                                    {
+                                        currentTime += 0.02;
+                                        chart1.Series["Temperature"].Points.AddXY(currentTime, currentTemp);
+                                    }
+                                }
+                                currentTime += 0.02;
+                                break;
+                            }
+                            else if (currentTemp <= nextTemp && compTemp == -10)
+                            {
+                                if (currentTemp != nextTemp)
+                                {
+                                    not_Enough();
+                                    currentTemp -= 1;
+                                    for (; currentTemp >= nextTemp; currentTemp = currentTemp - 1)
+                                    {
+                                        currentTime += 0.1;
+                                        chart1.Series["Temperature"].Points.AddXY(currentTime, currentTemp);
+                                    }
+                                    currentTemp += 1;
+                                    nextTemp = nextTemp + decimalValue;
+                                    currentTemp = (decimalValue != 0) ? currentTemp - 0.1 : currentTemp;
+                                    for (; currentTemp > nextTemp; currentTemp = currentTemp - 0.1)
+                                    {
+                                        currentTime += 0.01;
+                                        chart1.Series["Temperature"].Points.AddXY(currentTime, currentTemp);
+                                    }
+                                }
+                                currentTime += 0.01;
+                                break;
+                            }
+                            else
+                            {
+                                chart1.Series["Temperature"].Points.AddXY(currentTime, currentTemp);
+                                currentTemp = currentTemp + compTemp;
+                                currentTime = currentTime + 1;
+                                continue;
+                            }
+                        }
+                    }
+                    continue;
+                }
+                chart1.ChartAreas[0].AxisX.Maximum = Math.Ceiling(currentTime);
+            }
+        }
+
+        private void not_Enough()
+        {
+            currentTemp = currentTemp - compTemp;
+            decimalValue = Math.Round((nextTemp - currentTemp) % 1, 1);
+            wholeValue = Convert.ToInt32((nextTemp - currentTemp) / 1);
+            wholeValue = ((wholeValue + decimalValue) == (nextTemp - currentTemp)) ? wholeValue : wholeValue - 1;
+            nextTemp = nextTemp - decimalValue;
+            currentTime -= 1;
+        }
+
+
+        private double secToHour(double sec)
+        {
+            sec = Math.Round(sec / 3600,2);
+            return sec;
+        }
+        private double minToHour(double min)
+        {
+            min = Math.Round(min / 60,2);
+            return min;
+        }
+        private void ReadValues()
+        {
+            for (int step = 0; step < totalCount; step++)
+            {
+                object[] oValueArray;
+                short[] wQualityArray;
+                OpcRcw.Da.FILETIME[] fTimeArray;
+                int[] nErrorArray;
+                string[] target = { 
+                                      DEV_NAME + "." + WORD_REG_PREFIX + "" + (TEM_POS + (step + 1)), // get temp values
+                                      DEV_NAME + "." + WORD_REG_PREFIX + "" + (LOWER_TIME_POS + (step + 1)), // get time
+                                      DEV_NAME + "." + WORD_REG_PREFIX + "" + (LOWER_TIME_POS + STEP_OFFSET + (step + 1)), // get time
+                                      DEV_NAME + "." + BIT_REG_PREFIX + "" + (UNIT_BIT_POS + (step + 1)), // get unit seconds
+                                      DEV_NAME + "." + BIT_REG_PREFIX + "" + (UNIT_BIT_POS + STEP_OFFSET + (step + 1)), // get unit minutes
+                                      DEV_NAME + "." + BIT_REG_PREFIX + "" + (UNIT_BIT_POS + (STEP_OFFSET * 2) + (step + 1)), // get unit hours
+                                  };
+                try 
+                { 
+                    if (opc.Read(target, out oValueArray, out wQualityArray, out fTimeArray, out nErrorArray))
+                    {
+                        double rawValueTemp = Convert.ToDouble(oValueArray[0]);
+                        double newVal = minValue + ((maxValue - minValue) * (rawValueTemp - 0)) / (MAX_DIGIT - 0);
+                        data.Add(new DataCollection { 
+                                                         Step = step + 1, 
+                                                         Temperature = newVal != -100 ? Math.Round(newVal,1) : 0, 
+                                                         Time = Convert.ToInt32(oValueArray[1]) + (Convert.ToInt32(oValueArray[2]) * 10000),
+                                                         Unit = Convert.ToInt32(oValueArray[3]) == 1 ? "second" : Convert.ToInt32(oValueArray[4]) == 1 ? "minute" : "hour"
+                                                    }
+                                );
+                    }
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            currentTime = currentTemp = 0;
+            data.Clear();
+            chart1.Series["Temperature"].Points.Clear();
+            Initialize();
+            dataGridView1.SelectAll();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PrintingManager printManager = chart1.Printing;
+            chart1.Printing.PrintDocument.DefaultPageSettings.Margins.Top = 
+            chart1.Printing.PrintDocument.DefaultPageSettings.Margins.Left = 
+            chart1.Printing.PrintDocument.DefaultPageSettings.Margins.Bottom = 
+            chart1.Printing.PrintDocument.DefaultPageSettings.Margins.Right = 10;
+            printManager.PrintDocument.DefaultPageSettings.Landscape = true;
+            chart1.Printing.PrintPreview();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Png Image|*.png|JPeg Image|*.jpg|Bitmap Image|*.bmp";
+            save.Title = "Save the File";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                string fName = save.FileName;
+                this.chart1.SaveImage(fName, ChartImageFormat.Png);
+            }
+
+        }
+
+        //private void button4_Click(object sender, EventArgs e)
+        //{
+            //if (btnZoom.Text.Equals("Fit Graph"))
+            //{ 
+            //    double max=0;
+            //    double min=0;
+            //    for (int minMax = 0; minMax < totalCount; minMax++)
+            //    {
+            //        if (Convert.ToDouble((dataGridView1.Rows[minMax].Cells[1].Value)) > max)
+            //        {
+            //            max = Convert.ToDouble((dataGridView1.Rows[minMax].Cells[1].Value));
+            //        }
+            //        else if (Convert.ToDouble((dataGridView1.Rows[minMax].Cells[1].Value)) < min)
+            //        {
+            //            min = Convert.ToDouble((dataGridView1.Rows[minMax].Cells[1].Value));
+            //        }
+            //    }
+            //    chart1.ChartAreas[0].AxisY.Maximum = max;
+            //    chart1.ChartAreas[0].AxisY.Minimum = min;
+            //    chart1.ChartAreas[0].AxisY.ScaleView.Zoom(Math.Floor(min), Math.Ceiling(max));
+            //    chart1.ChartAreas[0].AxisX.Maximum = Math.Ceiling(currentTime);
+            //    chart1.ChartAreas[0].AxisX.ScaleView.Zoom(0, Math.Ceiling(currentTime)+1);
+            //    btnZoom.Text = "Unfit Graph";
+            //}
+            //else
+            //{
+            //    chart1.ChartAreas[0].AxisY.ScaleView.Zoom(0, 15);
+            //    chart1.ChartAreas[0].AxisX.ScaleView.Zoom(0, 10);
+            //    btnZoom.Text = "Fit Graph";
+            //}
+         //}
+    }
+}
