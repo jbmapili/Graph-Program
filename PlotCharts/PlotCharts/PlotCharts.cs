fe334ee704a729990d5526df80f72bafab322979
kumo203 <kumo203@gmail.com>
2014-10-08 13:38:13 +0800

    Graph-Program

--- PlotCharts/PlotCharts/PlotCharts.cs
+++ PlotCharts/PlotCharts/PlotCharts.cs
@@ -12,7 +12,7 @@ using OpcRcw.Da;
 
 namespace PlotCharts
 {
-    public partial class Form1 : Form
+    public partial class PlotCharts : Form
     {
         DxpSimpleAPI.DxpSimpleClass opc = new DxpSimpleAPI.DxpSimpleClass();
         public int count=0, compTemp, totalCount=20, point=0;
@@ -29,8 +29,11 @@ namespace PlotCharts
         const string WORD_REG_PREFIX = "D";
         const string BIT_REG_PREFIX = "M";
         const int MAX_DIGIT = 10000;
+
+        readonly string lineName = "温度"; //"Temperature";
+
         List<DataCollection> data = new List<DataCollection> { };
-        public Form1()
+        public PlotCharts()
         {
             InitializeComponent(); 
 
@@ -46,7 +49,7 @@ namespace PlotCharts
         {
             if (opc.Connect("localhost", "Takebishi.dxp"))
             {
-                chart1.ChartAreas[0].AxisY.ScaleView.Zoom(0, 15);
+                chart1.ChartAreas[0].AxisY.ScaleView.Zoom(0, 10);
                 dataGridView1.DefaultCellStyle.SelectionBackColor = dataGridView1.DefaultCellStyle.BackColor;
                 dataGridView1.DefaultCellStyle.SelectionForeColor = dataGridView1.DefaultCellStyle.ForeColor;
                 ReadValues();
@@ -58,12 +61,12 @@ namespace PlotCharts
                     {
                         //first value
                         currentTemp = Convert.ToDouble(dataGridView1.Rows[count].Cells[1].Value);
-                        point_Plot("start");
+                        point_Plot("Start");
 
                         //plot whole the next step
                         string unit = dataGridView1.Rows[count].Cells[3].Value.ToString();
-                        currentTime=currentTime+(toHour(Convert.ToDouble(dataGridView1.Rows[count].Cells[2].Value),unit.Equals("second")? 3600: unit.Equals("minute")? 60 : 1));
-                        point_Plot("end");
+                        currentTime = currentTime + (toHour(Convert.ToDouble(dataGridView1.Rows[count].Cells[2].Value), unit.Equals("秒") ? 3600 : unit.Equals("分") ? 60 : 1));
+                        point_Plot("End");
 
                         //search for next value
                         count++;
@@ -129,7 +132,7 @@ namespace PlotCharts
                             }
                             else
                             {
-                                chart1.Series["Temperature"].Points.AddXY(currentTime, currentTemp);
+                                chart1.Series[lineName].Points.AddXY(currentTime, currentTemp);
                                 currentTemp = currentTemp + compTemp;
                                 currentTime = currentTime + 1;
                                 point++;
@@ -140,20 +143,28 @@ namespace PlotCharts
                     continue;
                 }
                 chart1.ChartAreas[0].AxisX.Maximum = Math.Ceiling(currentTime);
+
+                //double min = data.Min(x => x.温度);
+                //double max = data.Max(x => x.温度);
+                //chart1.ChartAreas[0].AxisY.ScaleView.Zoom(min, max);
+
+                //double offset = (max - min) * 0.1;
+                //chart1.ChartAreas[0].AxisY.ScaleView.Zoom(min-offset, max+offset);
+                chart1.ChartAreas[0].AxisY.ScaleView.ZoomReset();
             }
         }
 
         private void add_Whole(double addValue)
         {
             currentTime += addValue;
-            chart1.Series["Temperature"].Points.AddXY(currentTime, currentTemp);
+            chart1.Series[lineName].Points.AddXY(currentTime, currentTemp);
             point++;
         }
 
         private void point_Plot(string startEnd)
-        {            
-            chart1.Series["Temperature"].Points.AddXY(currentTime, currentTemp);
-            chart1.Series["Temperature"].Points[point].Label = string.Format("{0}-{1}", count+1, startEnd);
+        {
+            chart1.Series[lineName].Points.AddXY(currentTime, currentTemp);
+            chart1.Series[lineName].Points[point].Label = string.Format("{0}-{1}", count + 1, startEnd);
             point++;
         }
 
@@ -195,10 +206,10 @@ namespace PlotCharts
                         double rawValueTemp = Convert.ToDouble(oValueArray[0]);
                         double newVal = minValue + ((maxValue - minValue) * (rawValueTemp - 0)) / (MAX_DIGIT - 0);
                         data.Add(new DataCollection { 
-                                                         Step = step + 1, 
-                                                         Temperature = newVal != -100 ? Math.Round(newVal,1) : 0, 
-                                                         Time = Convert.ToInt32(oValueArray[1]) + (Convert.ToInt32(oValueArray[2]) * 10000),
-                                                         Unit = Convert.ToInt32(oValueArray[3]) == 1 ? "second" : Convert.ToInt32(oValueArray[4]) == 1 ? "minute" : "hour"
+                                                         ステップ = step + 1, 
+                                                         温度 = newVal != -100 ? Math.Round(newVal,1) : 0, 
+                                                         時間 = Convert.ToInt32(oValueArray[1]) + (Convert.ToInt32(oValueArray[2]) * 10000),
+                                                         単位 = Convert.ToInt32(oValueArray[3]) == 1 ? "秒" : Convert.ToInt32(oValueArray[4]) == 1 ? "分" : "時間"
                                                     }
                                 );
                     }
@@ -212,18 +223,21 @@ namespace PlotCharts
             count = point = 0;
             currentTime = currentTemp = 0;
             data.Clear();
-            chart1.Series["Temperature"].Points.Clear();
+            chart1.Series[lineName].Points.Clear();
             Initialize();
-            dataGridView1.SelectAll();
+//            dataGridView1.SelectAll();
+            dataGridView1.Refresh();
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             PrintingManager printManager = chart1.Printing;
-            chart1.Printing.PrintDocument.DefaultPageSettings.Margins.Top = 
-            chart1.Printing.PrintDocument.DefaultPageSettings.Margins.Left = 
-            chart1.Printing.PrintDocument.DefaultPageSettings.Margins.Bottom = 
-            chart1.Printing.PrintDocument.DefaultPageSettings.Margins.Right = 10;
+            chart1.Printing.PrintDocument.DefaultPageSettings.Margins.Top 
+                = chart1.Printing.PrintDocument.DefaultPageSettings.Margins.Left 
+                = chart1.Printing.PrintDocument.DefaultPageSettings.Margins.Bottom  
+                = chart1.Printing.PrintDocument.DefaultPageSettings.Margins.Right 
+                = 10;
             printManager.PrintDocument.DefaultPageSettings.Landscape = true;
             chart1.Printing.PrintPreview();
         }
@@ -231,7 +245,7 @@ namespace PlotCharts
         private void button3_Click(object sender, EventArgs e)
         {
             SaveFileDialog save = new SaveFileDialog();
-            save.Filter = "Png Image|*.png|JPeg Image|*.jpg|Bitmap Image|*.bmp";
+            save.Filter = "Png Image|*.png";
             save.Title = "Save the File";
             if (save.ShowDialog() == DialogResult.OK)
             {
